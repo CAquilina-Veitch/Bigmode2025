085bd88b16c3bf6a6cec7176a6e2014bdb723ea7
Cody Aquilina-Veitch <codyav@westnet.com.au>
2025-01-25 11:31:29 +0800

    start audio manager

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Instance;
+
+        private float musicVolume = 1;
+        private float sfxVolume = 1;
+        private float gameVolume = 1;
+        
+        private void Awake()
+        {
+            Instance = this;
+        }
+    }
+}
