2c3032b35fd28c3e550c0c29aac5472034b614fe
Cody Aquilina-Veitch <codyav@westnet.com.au>
2025-01-26 21:28:39 +0800

    start boss

---
+++ Assets/Scripts/Enemy/BossBase.cs
@@ -0,0 +1,15 @@
+using Extensions;
+using UnityEngine;
+
+namespace Scripts.Enemy
+{
+    public class BossBase : DisposableBehaviour
+    {
+        
+    }
+
+    public class TestingFrogBoss : BossBase
+    {
+        
+    }
+}
